7305159b1e3f5b96d11d24019c10c94a3c7a38c7
Valentin Lafage <vlafage.dev@free.fr>
2024-11-08 19:17:20 +0100

    feat: add vim
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ ln -s $PWD/bash/.bashrc $HOME/.bashrc
 ln -s $PWD/bash/.bash_aliases $HOME/.bash_aliases
 ln -s $PWD/bash/.bash_variables $HOME/.bash_variables
 
+# editor
+ln -s $PWD/editor/.vimrc $HOME/.vimrc
+
